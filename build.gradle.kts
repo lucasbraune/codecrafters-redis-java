eede7befadf55e7384aaf1f6daec6563b367090c
Lucas Braune <lucasbraune@gamil.com>
2022-05-22 01:15:35 +0200

    Basic gradle setup

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    // Apply the application plugin to add support for building a CLI application in Java.
+    application
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("redis.clone.Main")
+}
