a7d1692e1f20731120f94d294ce6f6350dd4b3d5
Lucas Braune <lucasbraune@gamil.com>
2022-05-26 22:13:08 +0200

    Handle multiple clients with threads

--- src/main/java/codecrafters/redis/App.java
+++ src/main/java/codecrafters/redis/App.java
@@ -3,17 +3,20 @@ package codecrafters.redis;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Starting redis server.");
+        ExecutorService executor = Executors.newCachedThreadPool();
         int port = 6379;
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             serverSocket.setReuseAddress(true);
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 Runnable worker = new RequestHandler(clientSocket);
-                worker.run();
+                executor.execute(worker);
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
