825e5c4452167667485a3f0399a5af821e681c50
Lucas Braune <lucasbraune@gamil.com>
2022-05-26 19:28:40 +0200

    Remove unused imports

--- src/main/java/codecrafters/redis/App.java
+++ src/main/java/codecrafters/redis/App.java
@@ -3,8 +3,6 @@ package codecrafters.redis;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class App {
     public static void main(String[] args) {
