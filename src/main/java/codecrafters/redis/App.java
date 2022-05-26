eef85252d808d891ae7fb5a85d258297f4df4857
Lucas Braune <lucasbraune@gamil.com>
2022-05-26 22:00:45 +0200

    Move request handling to separate class

--- src/main/java/codecrafters/redis/App.java
+++ src/main/java/codecrafters/redis/App.java
@@ -1,6 +1,6 @@
 package codecrafters.redis;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,39 +12,11 @@ public class App {
             serverSocket.setReuseAddress(true);
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                handleRequests(clientSocket);
+                Runnable worker = new RequestHandler(clientSocket);
+                worker.run();
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-        }
-    }
-
-    /**
-     * Handles requests from a socket, then closes it.
-     */
-    private static void handleRequests(Socket clientSocket) throws IOException {
-        // Closing a socket's input or output stream closes the socket.
-        try (InputStream in = new BufferedInputStream(clientSocket.getInputStream());
-             OutputStream out = clientSocket.getOutputStream()
-        ) {
-            for (RespArray request = RespArray.decode(in);
-                 request != null;
-                 request = RespArray.decode(in)
-            ) {
-                RespData response = handleRequest(request);
-                out.write(response.encode().getBytes());
-            }
-        } catch (InputMismatchException e) {
-            System.out.println("Unable to parse request: " + e.getMessage());
-        }
-    }
-
-    private static RespData handleRequest(RespArray request) {
-        String command = request.getElements().get(0).getValue();
-        if (command.equals("ping")) {
-            return new RespSimpleString("PONG");
-        } else {
-            return new RespSimpleString("Unknown command: " + command);
         }
     }
 }
---
+++ src/main/java/codecrafters/redis/RequestHandler.java
@@ -0,0 +1,48 @@
+package codecrafters.redis;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class RequestHandler implements Runnable {
+
+    private final Socket clientSocket;
+
+    public RequestHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    /**
+     * Handles requests from this Request Handler's socket, then closes it.
+     */
+    @Override
+    public void run() {
+        // Closing a socket's input or output stream closes the socket.
+        try (InputStream in = new BufferedInputStream(clientSocket.getInputStream());
+             OutputStream out = clientSocket.getOutputStream()
+        ) {
+            for (RespArray request = RespArray.decode(in);
+                 request != null;
+                 request = RespArray.decode(in)
+            ) {
+                RespData response = handleRequest(request);
+                out.write(response.encode().getBytes());
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Unable to parse request: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+
+    private static RespData handleRequest(RespArray request) {
+        String command = request.getElements().get(0).getValue();
+        if (command.equals("ping")) {
+            return new RespSimpleString("PONG");
+        } else {
+            return new RespSimpleString("Unknown command: " + command);
+        }
+    }
+}
